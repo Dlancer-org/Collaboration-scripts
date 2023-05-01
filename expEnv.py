b6ac86502b334f9cfe22e1f043764ee749b599fe
faiz-hmed <faiz.ahmeds2000@gmail.com>
2023-05-01 21:32:33 +0530

    Initial commit with script for testing

---
+++ expEnv.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import json
+
+# task_id  = os.environ['TASK_ID']
+# res = requests.get('http://localhost:5000/taskTrackingInfo/' + task_id)
+# data = res.json()
+
+# test_dest_path = data['test_dest_path']
+# tests = data['test_cases']
+print(os.getcwd())
+
+
+# os.environ['TEST_SUITE'] = data['test_cases']
+# os.environ['TEST_RUNNER'] = data['test_runner']
