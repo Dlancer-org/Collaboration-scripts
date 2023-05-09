c5eb84dddf8ff7f7557c15dd26d0e94df9bc9b3c
faiz-hmed <faiz.ahmeds2000@gmail.com>
2023-05-10 00:49:22 +0530

    Changed worflow, exporter & submit scripts

--- expEnv.py
+++ expEnv.py
@@ -1,68 +1,68 @@
-import subprocess
 import requests
 import os
 import sys
 import argparse
 import base64
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--prTitle', help='Taskid of the task submitted')
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--prTitle', help='Taskid of the task submitted')
+args = parser.parse_args()
 
-pr_title = os.environ["prTitle"]
+pr_title = args.prTitle
 split_title = pr_title.split(';')
 task_id = split_title[-1]
 
-res = requests.get('https://bae7-106-51-25-38.ngrok-free.app/api/gh/task/' + task_id)
+res = requests.get('https://26da-103-148-39-221.ngrok-free.app/api/gh/task/' + task_id)
 data = res.json()
+print(res)
 
 test_dest_path = data['test_dest_path']
 dep_installer = data['dep_installer']
 test_dest_file_name = data['test_dest_file_name']
-# tests =  + "\n" + data['hidden_tests']
-
-os.chdir(test_dest_path)
 
 if(os.path.isfile(test_dest_path)):
-    test_file = open(test_dest_path, 'a')
-    test_file.write(data['open_tests'] + "\n")
-    test_file.write(data['hidden_tests'])
-    test_file.close()
+    with open(test_dest_path, 'a') as test_file:
+        test_file.write(data['open_tests'] + "\n")
+        test_file.write(data['hidden_tests'])
+    
 else:
     try:
-        test_file = open(os.path.join(test_dest_path, test_dest_file_name) , 'x')
+        with open(os.path.join(test_dest_path, test_dest_file_name) , 'x') as test_file:
+            test_file.write(data['open_tests'] + "\n")
+            test_file.write(data['hidden_tests'])
     except:
-        print("File with given name already exists")
+        print("File with given name already exists / Invalid path to embedded given tests")
         sys.exit(1)
 
-    test_file.write(data['open_tests'] + "\n")
-    test_file.write(data['hidden_tests'])
-    test_file.close()
+workflow_initial = requests.get('https://raw.githubusercontent.com/Dlancer-org/Collaboration-scripts/master/template.yml')
+workflow_initial = workflow_initial.content
+workflow_initial = workflow_initial.decode('utf-8')
 
-# workflow_initial = requests.get('https://raw.githubusercontent.com/Dlancer-org/Collaboration-scripts/master/template.yml')
-# workflow_initial = workflow_initial.content
-# workflow_initial = workflow_initial.decode('utf-8')
+workflow_initial_b64 = workflow_initial.encode('ascii')
+workflow_initial_b64 = base64.b64encode(workflow_initial_b64)
 
-# workflow_initial_b64 = workflow_initial.encode('ascii')
-# workflow_initial_b64 = base64.b64encode(workflow_initial_b64)
+workflow_current = open(os.path.join('.github','workflows','Dlancer-Integration.yml'), 'r').read()
+workflow_current_b64 = workflow_current.encode('ascii')
+workflow_current_b64 = base64.b64encode(workflow_current_b64)
 
-# workflow_current = open(os.path.join('.github','workflows','Dlancer-Integration.yml'), 'r').read()
-# workflow_current_b64 = workflow_current.encode('ascii')
-# workflow_current_b64 = base64.b64encode(workflow_current_b64)
+if(workflow_initial_b64 != workflow_current_b64):
+    print("Workflow file has been changed. Please update the workflow file to the initial state")
+    sys.exit(1)
 
-# if(workflow_initial_b64 != workflow_current_b64):
-#     print("Workflow file has been changed. Please update the workflow file to the initial state")
-#     sys.exit(1)
+with open(os.path.join('.', 'dep_ins.sh') , 'w') as dep:
+    dep.write('#!/bin/bash\n')
+    dep.write(str(data["dep_installer"]))
 
-subprocess.run(f'export PR_TITLE="{pr_title}"', shell=True)
-subprocess.run(f'export DEP_INSTALL_CMD="{dep_installer}"', shell=True)
-# subprocess.run(f'export TEST_SUITE="{tests}"', shell=True)
-subprocess.run(f'export TEST_RUNNER="{data["test_runner"]}"', shell=True)
-subprocess.run(f'export TEST_DEST_PATH="{test_dest_path}"', shell=True)
-subprocess.run(f'export TEST_DEST_FILE_NAME="{test_dest_file_name}"', shell=True)
-subprocess.run(f'export TASK_ID="{task_id}"', shell=True)
+with open(os.path.join('.', 'test.sh') , 'w') as test:
+    test.write('#!/bin/bash\n')
+    test.write(str(data["test_runner"]))
 
+os.environ["PR_TITLE"] = ''.join(split_title[:-1])
+os.environ["TASK_ID"] = task_id
 
-
-cmd = 'echo -e "TEST_SUITE=$TEST_SUITE\nTEST_RUNNER=$TEST_RUNNER\nTEST_DEST_PATH=$TEST_DEST_PATH\nTASK_ID=$TASK_ID\nDEP_INSTALL_CMD=$DEP_INSTALL_CMD\nPR_TITLE=$PR_TITLE\nTEST_DEST_FILE_NAME:$TEST_DEST_FILE_NAME" >> $GITHUB_ENV'
-os.system(cmd)
+try:
+    cmd = 'echo "TASK_ID=$TASK_ID\nPR_TITLE=$PR_TITLE\n" >> file1.txt'
+    os.system(cmd)
+except:
+    print("Error while exporting environment variables")
+    sys.exit(1)
--- handleSubmit.py
+++ handleSubmit.py
@@ -1,41 +1,44 @@
-# To submit : repoName, repoOwner, taskid, prAuthor, prNum, prTitle, prDescription, tests, testDestPath, testDestFileName
+# To submit : repoName, repoOwner, taskid, prAuthor, prNum, prTitle, prDescription
 import requests
 import os
 import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--prAuthor', help='Github username of the Pull request author')    # ${{ github.event.pull_request.user.login }}
-# parser.add_argument('--prNum', help='Number attached to the pull request')              # ${{ github.event.pull_request.number }}
-# parser.add_argument('--prDescription', help='Title of the pull request')                # ${{ github.event.pull_request.body }}
-# parser.add_argument('--repoDetails')                                                    # ${{ github.repository }}
+parser.add_argument('--prAuthor', help='Github username of the Pull request author')    # ${{ github.event.pull_request.user.login }}
+parser.add_argument('--prNum', help='Number attached to the pull request')              # ${{ github.event.pull_request.number }}
+parser.add_argument('--prDescription', help='Title of the pull request')                # ${{ github.event.pull_request.body }}
+parser.add_argument('--repoDetails')                                                    # ${{ github.repository }}
 
-# args = parser.parse_args()
+args = parser.parse_args()
+host = os.environ['HOST']
 
-prAuthor = os.environ['prAuthor']
-prNum = os.environ['prNum']
-prDescription = os.environ['prDescription']
-repoDetails = os.environ['repoDetails']
+prAuthor = args.prAuthor
+prNum = args.prNum
+prDescription = args.prDescription
+repoDetails = args.repoDetails
+
+# prAuthor = os.environ['prAuthor']
+# prNum = os.environ['prNum']
+# prDescription = os.environ['prDescription']
+# repoDetails = os.environ['repoDetails']
 
 repoName = repoDetails.split('/')[1]
 repoOwner = repoDetails.split('/')[0]
 
-response = requests.post('https://bae7-106-51-25-38.ngrok-free.app/api/gh/task', json={
+response = requests.post(f'{host}/api/gh/task', json={
     'repoName': repoName,
     'repoOwner': repoOwner,
     'taskid': os.environ['TASK_ID'],
     'prAuthor': prAuthor,
     'prNum': prNum,
     'prTitle': os.environ['PR_TITLE'],
-    'prDescription': prDescription,
-    'tests': os.environ['TEST_SUITE'],
-    'testDestPath': os.environ['TEST_DEST_PATH'],
-    'testDestFileName': os.environ['TEST_DEST_FILE_NAME']
+    'prDescription': prDescription
 })
 
-if(response.status_code == 200):
+try:
+    response.raise_for_status()
     print("Task submitted successfully")
-    sys.exit(0)
-else:
-    print("Task submission failed")
+except requests.exceptions.HTTPError as err:
+    print("Task submission failed", err)   
     sys.exit(1)
